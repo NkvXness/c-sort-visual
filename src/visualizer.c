9e175f048334cab8e2bf619489e4707080e6d209
NkvXness <leonakavexmusic@gmail.com>
2026-02-04 12:18:53 +0300

    Add ElementRole enum for coloring bar states
    NORMAL, COMPARING, SWAPPING, SORTED, PIVOT

--- src/visualizer.c
+++ src/visualizer.c
@@ -1,21 +1,37 @@
 #include "visualizer.h"
 #include <stdlib.h>
+#include <string.h>
 
-static const Color COL_NORMAL = { 72, 108, 196, 255 };
+static const Color COL_NORMAL  = {  72, 108, 196, 255 };
+static const Color COL_COMPARE = { 238, 210,  60, 255 };
+static const Color COL_SWAP    = { 228,  58,  58, 255 };
+static const Color COL_SORTED  = {  56, 200,  96, 255 };
+static const Color COL_PIVOT   = { 196,  72, 220, 255 };
+
+static Color role_to_color(int role) {
+    switch (role) {
+        case ROLE_COMPARING: return COL_COMPARE;
+        case ROLE_SWAPPING:  return COL_SWAP;
+        case ROLE_SORTED:    return COL_SORTED;
+        case ROLE_PIVOT:     return COL_PIVOT;
+        default:             return COL_NORMAL;
+    }
+}
 
 VisState *vis_create(int size) {
     VisState *vs = malloc(sizeof(VisState));
-    vs->size = size;
+    vs->size  = size;
+    vs->roles = calloc(size, sizeof(int));
     return vs;
 }
 
 void vis_free(VisState *vs) {
+    free(vs->roles);
     free(vs);
 }
 
 void vis_draw_bars(const Array *arr, const VisState *vs,
                    int ox, int oy, int w, int h) {
-    (void)vs;
     float bar_w = (float)w / arr->size;
     float gap   = (bar_w > 3.0f) ? 1.0f : 0.0f;
     for (int i = 0; i < arr->size; i++) {
@@ -25,6 +41,7 @@ void vis_draw_bars(const Array *arr, const VisState *vs,
         int   by    = oy + h - bar_h;
         int   bw    = (int)(bar_w - gap);
         if (bw < 1) bw = 1;
-        DrawRectangle(bx, by, bw, bar_h, COL_NORMAL);
+        Color col = role_to_color(vs->roles[i]);
+        DrawRectangle(bx, by, bw, bar_h, col);
     }
 }
--- src/visualizer.h
+++ src/visualizer.h
@@ -4,8 +4,17 @@
 #include "array.h"
 #include <raylib.h>
 
+typedef enum {
+    ROLE_NORMAL = 0,
+    ROLE_COMPARING,
+    ROLE_SWAPPING,
+    ROLE_SORTED,
+    ROLE_PIVOT,
+} BarRole;
+
 typedef struct {
-    int  size;
+    int  *roles;
+    int   size;
 } VisState;
 
 VisState *vis_create(int size);
